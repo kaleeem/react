63feaf2d585277bf2e7d24464b8e541b635c2706
kaleem <116940311+kaleeem@users.noreply.github.com>
2023-01-11 23:05:52 -0800

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,38 +1,58 @@
-import React, {useState} from 'react';
+import React,{useState} from 'react';
 
-
-function Main() {
-    const [firstName,setFirstName]=useState('')
-    const [lastName,setLastName]=useState('')
+function Main(){
+    const [Name,setName]=useState('')
+    const [Password,setPassword]=useState('')
     const [error,setError]=useState(false)
-    const[submitted,setSubmitted]=useState(false)
-    const handleSubmit=(e)=>{
-        e.preventDefault();
-        if(firstName.length==0||lastName.length==0){
-            setError(true)
+    const [submitted,setSubmitted]=useState(false)
+    const validdata=(e)=>{
+        e.preventDefault()
+        if(Name.length==0){
+            setError(true);
+        }
+        else if(Password.length<=6){
+            setError(true);
         }
         else{
             setSubmitted(true)
         }
     }
-    const sucess= () =>{
-        if(submitted)
-        return(
-            <div>
-                <h1>Sucessfully Registered {firstName} </h1>
-            </div>
-        );
+    const sucess=()=>{
+        if(submitted){
+            return(
+                <div>
+                    <h3>Sucessfull Registered {Name}</h3>
+                </div>
+            )
+        }
     }
-    return (
-        <div>
+
+
+    return(
         <>
-           <form onSubmit={handleSubmit} action="valid.html" method='post'>
-               <div>
-                <h1>Registraton</h1>
-                   <input placeholder="First Name" onChange={e=>setFirstName(e.target.value)} />
-               </div>
-               {error&&firstName.length<=0?
-               <label>First Name can't be Empty</label>:""}
+        <form onSubmit={validdata}>
+            <h2>Rigstration</h2>
+            <input type="text" placeholder='Name'  onChange={e=>setName(e.target.value)}/>
+            <div className='err'>
+                {error&&Name.length==0? <label>Enter Youre Name</label>:""}
+            </div>
+            <input type="password" onChange={e=>setPassword(e.target.value)} />
+            <div className='err'>
+                {error&&Password.length<=6? <label>Password Must Be greater than 6</label>:""}
+            </div>
+            <button className='btt'>
+                Submit
+            </button >
+            <button type='reset'>Reset</button>
+            <div>
+                {sucess()}
+            </div>
+
+        </form>
+        </>
+    )
+}
+export default Main;
                <div>
                    <input placeholder="Last Name" onChange={e=>setLastName(e.target.value)} />
                </div>
@@ -52,3 +72,24 @@ function Main() {
  )
 }                  
 export default Main;
+
+
+// form{
+//   text-align: center;
+//   position: relative;
+//   top: 30vh;
+//   border: 2px solid;
+//   width: 300px;
+//   left: 37vw;
+// }
+// input{
+//   width: 17vw;
+//   height: 2.7vh;
+// }
+// .err{
+//   padding: 8px;
+// }
+// .btt{
+//   position: relative;
+//   right: 1.2vw;
+// }
